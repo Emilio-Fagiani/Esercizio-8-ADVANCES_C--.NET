aa87580739a66ab88bbae05e55e0c8e461519a21
emifa <emifa@LAPTOP-1TM449QB>
2023-07-20 14:58:18 +0200

    Aggiungere i file di progetto.

---
+++ Esercizio-8-ADVANCES_C--.NET/IEnqueue.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esercizio_8_ADVANCES_C__.NET
+{
+    internal interface IEnqueue<T>
+    {
+          T Enqueue(T item);
+    }
+}
---
+++ Esercizio-8-ADVANCES_C--.NET/MyQueue.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esercizio_8_ADVANCES_C__.NET
+{
+    public class MyQueue<T> : IEnqueue<T>, IDequeue<T>, IPeek<T>
+    {
+
+        public Queue<T> queue = new Queue<T>();
+
+        public T Dequeue()
+        {
+            if (queue == null)
+                throw new NotImplementedException();
+            return queue.Dequeue();
+        }
+
+        public T Enqueue(T item)
+        {
+           queue.Enqueue(item); return item;
+        }
+
+        public  T Peek()
+        {
+            if (queue.Count == 0)
+                throw new InvalidOperationException("The queue is empty.");
+            return queue.Peek();
+        }
+    }
+}
